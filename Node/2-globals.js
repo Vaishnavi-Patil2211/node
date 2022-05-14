d8acad417d3218a038dda2e9f55efd2412bf8221
vaishnavi patil <vp397264@gmail.com>
2022-05-14 17:46:55 +0530

    new file

---
+++ Node/2-globals.js
@@ -0,0 +1,11 @@
+// __dirname - path to current directory
+//__filename - file name
+// require - function to use modules (commonJS)
+// module - info about current module (file)
+// process - info about env where the program is being executed
+
+
+console.log(__dirname)
+setInterval(() => {
+    console.log('hello world')
+},1000)
