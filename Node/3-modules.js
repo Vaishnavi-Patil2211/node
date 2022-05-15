2196558cc81234c4b1d3434559aeaea7952687b6
vaishnavi patil <vp397264@gmail.com>
2022-05-15 22:18:26 +0530

    new

---
+++ Node/3-modules.js
@@ -0,0 +1,13 @@
+//modules
+
+const names = require('./4-names');
+const sayhi = require('./5-utiles')
+
+const data = require('./6-alternative-flavor')
+require('./7-mind-grenade')
+
+
+
+// sayhi('susan')
+// sayhi(names.john)
+// sayhi(names.peter)
