8a5bbb0181d91f619ad8ee8e235b57096fc5b7bd
vaishnavi patil <vp397264@gmail.com>
2022-05-29 23:11:21 +0530

    new

---
+++ Node/13-event-emitter.js
@@ -0,0 +1,27 @@
+
+
+
+
+//get back the class
+//if want custom extend from class
+//otherwise just for emmiting and handling events create instance 
+
+const EventEmitter =require('events');
+
+const customEmitter = new EventEmitter()
+
+//on and emit methods
+//keep track of the order
+//additional arguments
+//built-in modules utilize it
+
+
+
+
+customEmitter.on('response', (name,id) => {
+    console.log(`data recieved user ${name} with id:${id}`)
+})
+customEmitter.on('response', () => {
+    console.log(`some other logic here`)
+})
+customEmitter.emit('response','john' ,34)
